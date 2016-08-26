08b74a12b728f49b94dbe8403feab7a7b51e576e
Dave Hanlon <dave@mumfordland.com>
2016-08-26 08:09:59 -0500

    Initial commit of the bot repo

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    consumer_key:        'sticymoTj9cKszaTWQQGrr4Rm',    
+    access_token:        '768806591004971008-SZceIh62F8wmLyOtPuLMfFbAoqmngtC'
+}
+
+module.exports = config;
---
+++ server.js
@@ -0,0 +1,59 @@
+var fs = require('fs'),
+    path = require('path'),
+    Twit = require('twit'),
+    Morse = require('morse-node'),
+    moment = require('moment'),
+    config = require(path.join(__dirname, 'config.js')),
+    messages = require(path.join(__dirname, 'messageConfig.json'));
+
+moment().format();
+
+config.consumer_secret = process.env.consumer_secret;
+config.access_token_secret = process.env.access_token_secret;
+
+var twit = new Twit(config);
+var morse = Morse.create('ITU');
+var lastPosted;
+
+setInterval(function() {   
+    var postTimeToday = moment(messages.repeatingMessages.postTime, "HH:mm:ss");
+
+    console.log(postTimeToday.format() + " < " + moment().format());    
+
+    if (!lastPosted ){
+        //|| (lastPosted.dayOfYear() < moment().dayOfYear()         
+        //&& postTimeToday.isBefore(moment()))) {
+        var index = Math.round(Math.random() * messages.repeatingMessages.messages.length);
+
+        console.log("Retrieving message " + index + " of " +
+            messages.repeatingMessages.messages.length);
+
+        var message = morse.encode(messages.repeatingMessages.messages[index - 1]);
+
+        twit.post('statuses/update', { status: message },
+            function(err, data, response) {
+                console.log(data);
+                lastPosted = moment();
+                console.log("Last message posted at " + lastPosted.format());
+            });
+    }
+}, 60000*60);
+
+/*
+var message = morse.encode("Testing out morse node");
+
+console.log("Message length is " + message.length + " characters.");
+
+if (message.length <= 140) {
+    console.log(message);
+} else {
+    console.log("Message length exceeds allowable limit.");
+}
+*/
+
+/*
+twit.post('statuses/update', { status: '.. / .- -- / ... - .. .-.. .-.. / .... . .-. .' },
+    function(err, data, response) {
+        console.log(data);}
+    );
+*/
