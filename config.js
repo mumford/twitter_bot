48a1fc721eec02ecae758cb71cc67d86c111a3ea
Dave Hanlon <david.hanlon@mlb.com>
2016-09-09 12:36:17 -0500

    Switching back to production mode

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
     consumer_key:       'sticymoTj9cKszaTWQQGrr4Rm',    
     access_token:       '768806591004971008-SZceIh62F8wmLyOtPuLMfFbAoqmngtC',
-    inDevelopmentMode:  true,
+    inDevelopmentMode:  false,
     timezone:           'America/Chicago',
     defaultLoopTimeInSeconds:    10,
     awsRegion:          'us-east-1',
